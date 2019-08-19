bf13d933a8c0f5a8fa33ce2d61c3a22ba51ccf4b
sabdi21 <sabdi21@hotmail.com>
2019-08-19 08:59:22 -0700

    initial commit with favs done

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const express = require('express');
 const axios = require('axios'); 
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
-const port = process.env.PORT || 3000;
+var db = require('./models'); // Make sure to require your models in the files where they will be used.
+const request = require('request');
 
 app.use(require('morgan')('dev'));
 app.set('view engine', 'ejs');
@@ -14,17 +15,20 @@ app.use(ejsLayouts);
 app.get('/', function(req, res) {
   var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/';
   // Use request to call the API
-  axios.get(pokemonUrl).then( function(apiResponse) {
-    var pokemon = apiResponse.data.results;
+  request(pokemonUrl, (error, response, body) => {
+    var pokemon = JSON.parse(body).results;
+    console.log(pokemon);
     res.render('index', { pokemon: pokemon.slice(0, 151) });
   })
+  // axios.get(pokemonUrl).then( function(apiResponse) {
+  //   var pokemon = apiResponse.data.results;
+  //   res.render('index', { pokemon: pokemon.slice(0, 151) });
+  // })
 });
 
 // Imports all routes from the pokemon routes file
 app.use('/pokemon', require('./routes/pokemon'));
 
-var server = app.listen(port, function() {
-  console.log('...listening on', port );
-});
+var server = app.listen(process.env.PORT || 3000);
 
 module.exports = server;
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,43 @@
 var express = require('express');
 var router = express.Router();
+require('dotenv').config();
+var db = require('../models');
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', function(req, res) {
   // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+  db.pokemon.findAll()
+  .then((poke) => 
+    res.render('pokemon/show', { poke})
+  )
+  .catch((err) => {
+    console.log('ERROR HAPPENED', err)
+  })
+});
+
+// GET /pokemon - return a page with favorited Pokemon
+router.get('/', function(req, res) {
+  // TODO: Get all records from the DB and render to view
+  db.pokemon.findAll()
+  .then((poke)=>{
+    res.render('pokemon/show',{ poke})
+  })
+  .catch((err)=>{
+    console.log('err',err)
+    res.render('404')
+  })
 });
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
-router.post('/', function(req, res) {
+router.post('/', function(req, res,) {
   // TODO: Get form data and add a new record to DB
-  res.send(req.body);
+  db.pokemon.create(req.body)
+  .then((poke) => {
+    res.redirect('pokemon');
+  })
+  .catch((err) => {
+    console.log('ERROR HAPPENED', err)
+  })
 });
 
 module.exports = router;
